04f102c319811208359f58c82caca6dd086923b1
Corey Ward <corey.atx@gmail.com>
2019-04-06 21:27:02 -0500

    Expose FormContext

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export default from "./ValidatedForm"
+export { FormContext } from "./ValidatedForm"
 export { default as useFormField } from "./useFormField"
 export { default as validateEmail } from "./validators/email"
